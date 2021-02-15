f0485d7952ef563e209ffd10c8b22c43b43bf5d4
calmighted <calmighted@gmail.com>
2021-02-15 11:11:23 +0530

    layout and css completed

---
+++ src/projects.js
@@ -0,0 +1,6 @@
+//functionalities
+//1.Delete a project
+//2.Rename a project
+//3.show its todos upon click
+//4.Add a todo
+//5.project description
---
+++ src/todo.js
@@ -0,0 +1,4 @@
+//1.edit 
+//2.set date
+//3.priority(op)
+
